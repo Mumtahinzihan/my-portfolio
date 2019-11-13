e45c2b91b336658473cfccb577d575029c76e28c
Mumtahinzihan <mumtahin@yahoo.com>
2019-11-13 12:40:48 +0000

    header updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import facebookLogo from './images/facebook-logo.png';
+import githubLogo from './images/github-logo.png';
+import instagramLogo from './images/instagram-logo.png';
+import linkedinLogo from './images/linkedin-logo.jpg';
+import emailLogo from './images/emailLogo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header class='cover'>
+        <span><img src={facebookLogo} alt='facebookLogo'/></span>
+        <span><img src={githubLogo} alt='githubLogo'/></span>
+        <span><img src={instagramLogo} alt='instagramLogo'/></span>
+        <span><img src={linkedinLogo} alt='linkedinLogo'/></span>
+        <span><img src={emailLogo} alt='emailLogo'/></span>
       </header>
     </div>
   );
